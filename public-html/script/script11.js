f963d6a1b5a03c6913c1dff9a4d15127bb49294a
saulocaceresg <scaceresg@unsa.edu.pe>
2025-11-08 18:26:40 -0500

    Commit 14: script11.js -> ejercicio 15 terminado

--- public-html/script/script11.js
+++ public-html/script/script11.js
@@ -7,7 +7,7 @@
 console.log("Ejercicio 12 (14.)");
 document.write("<h3>Ejercicio 12 - Herencia</h3>");
 
-let clientes = []; // Array para almacenar clientes
+let clientes = []; // Array para almacenar clientes y administradores
 
 // Clase PADRE
 class Usuario {
@@ -22,6 +22,10 @@ class Usuario {
     }
     get email() {
         return this.#email;
+    }
+    mostrarInfo() {
+        console.log("Usuario: " + this.nombre + "\nE-mail: " + this.email + "\n-------------------");
+        document.write(`<p><b>Usuario:</b> ${this.nombre}<br><b>E-mail:</b> ${this.email}</p>`);
     }
 }
 
@@ -49,39 +53,37 @@ class Administrador extends Usuario {
         this.#permiso = permiso;
     }
     mostrarInfo () {
-        switch (this.#permiso) {
-            case "crear":
-                console.log("Creando cliente nuevo...");
-                document.write("<hr><h4>Administrador - Crear Cliente</h4>");
-                const nuevoCliente = new Cliente("Ana", "ana@gmail.com", 3);
-                clientes.push(nuevoCliente);
-                nuevoCliente.mostrarInfo();
-                console.log("¡Cliente creado exitosamente!");
-                document.write("<p>¡Cliente creado exitosamente!</p>");
-                break;
-            case "editar":
-                console.log("Editando cliente " + this.cliente.nombre + "...");
-                document.write("<hr><h4>Administrador - Editar Cliente</h4>");
-                this.cliente = new Cliente("Pablo", "asas@gmail.com", 3);
-                this.cliente.mostrarInfo();
-                console.log("¡Cliente editado exitosamente!");
-                document.write("<p>¡Cliente editado exitosamente!</p>");
-                break;
-            case "eliminar":
-                console.log("Eliminando cliente " + this.cliente.nombre + "...");
-                document.write("<hr><h4>Administrador - Eliminar Cliente</h4>");
-                clientes = clientes.filter(c => c !== this.cliente); // Filtrar cliente eliminado
-                this.cliente = null; // Primero filtra y luego elimina la referencia
-                // clientes.pop();
-                console.log("¡Cliente eliminado exitosamente!");
-                document.write("<p>¡Cliente eliminado exitosamente!</p>");
-                break;
-            default:
-                console.log("Permiso no válido.");
-                break;
-        }
+        console.log("ADMINISTRADOR\nNombre: " + this.nombre + "\nE-mail: " + this.email + "\nPermiso: " + this.#permiso);
+        document.write(`<h5>ADMINISTRADOR</h5><p><b>Nombre:</b> ${this.nombre}<br><b>E-mail:</b> ${this.email}<br><b>Permiso:</b> ${this.#permiso}</p>`);
+    }
+    crear () {
+        console.log("Creando cliente nuevo...");
+        document.write("<hr><h4>Administrador - Crear Cliente</h4>");
+        const nuevoCliente = new Cliente("Ana", "ana@gmail.com", 3);
+        clientes.push(nuevoCliente);
+        nuevoCliente.mostrarInfo();
+        console.log("¡Cliente creado exitosamente!");
+        document.write("<p>¡Cliente creado exitosamente!</p>");
+    }
+    editar () {
+        console.log("Editando cliente " + this.cliente.nombre + "...");
+        document.write("<hr><h4>Administrador - Editar Cliente</h4>");
+        this.cliente = new Cliente("Pablo", "asas@gmail.com", 3);
+        this.cliente.mostrarInfo();
+        console.log("¡Cliente editado exitosamente!");
+        document.write("<p>¡Cliente editado exitosamente!</p>");
+    }
+    eliminar () {
+        console.log("Eliminando cliente " + this.cliente.nombre + "...");
+        document.write("<hr><h4>Administrador - Eliminar Cliente</h4>");
+        clientes = clientes.filter(c => c !== this.cliente); // Filtrar cliente eliminado
+        this.cliente = null; // Primero filtra y luego elimina la referencia
+        // clientes.pop();
+        console.log("¡Cliente eliminado exitosamente!");
+        document.write("<p>¡Cliente eliminado exitosamente!</p>");
     }
 }
+
 
 // Clientes de prueba
 const cliente1 = new Cliente("Saulo", "saulo@gmail.com", 4);
@@ -103,22 +105,32 @@ cliente4.mostrarInfo();
 // Administrador de prueba. Se crea un cliente por defecto
 const admin = new Administrador("Melquiades", "melq@gmail.com", "crear", cliente1);
 
-admin.mostrarInfo();
+admin.crear(); // Crear nuevo cliente
 
 // Propiedades editar y eliminar
 console.log("=========== Cambiando permiso a editar ===========");
 
 admin.permiso = "editar";
-admin.mostrarInfo();
+admin.editar();
 
 admin.cliente = cliente2; // Se cambia de cliente para poder eliminar
 
 console.log("=========== Cambiando permiso a eliminar ===========");
 admin.permiso = "eliminar";
-admin.mostrarInfo();
+admin.eliminar();
 
-console.log("========= Lista de clientes actuales =========");
+
+
+// 15.	Polimorfismo. Crear una lista de usuarios (Cliente, Administrador) y recórrela mostrando la información con mostrarInfo(). Tip: usa un forEach o for...of para recorrer el array
+
+console.log("Ejercicio 13 (15.)");
+document.write("<h3>Ejercicio 13 - Polimorfismo</h3>");
+
+let usuarios = [new Cliente("Desamparados", "desa@gmail.com", 3), new Cliente("Socorro", "socorro@gmail.com", 5), new Administrador("Feliciano", "feliciano@gmail.com", "crear")];
+
+console.log("\n========= Lista de clientes actuales =========");
 document.write("<hr><h3>Lista de clientes actuales</h3>");
-for (let i of clientes) {
-    i.mostrarInfo();
+
+for (let j of usuarios) {
+    j.mostrarInfo();
 }
