30a1ddcbc7e3aacec3bd0bc794a68f165f4d6f93
saulocaceresg <scaceresg@unsa.edu.pe>
2025-11-08 19:06:50 -0500

    Commit 15: script12.js terminado

---
+++ public-html/script/script12.js
@@ -0,0 +1,67 @@
+// // 16.	Herencia. Crear la jerarquía Empleado - Programador - ProgramadorSenior 
+// •	Empleado tiene nombre y sueldoBase 
+// •	Programador añade lenguaje y método calcularSueldo() con bono del 10% 
+// •	ProgramadorSenior sobreescribe calcularSueldo() con un bono del 25%
+//      Tip: llama a super.calcularSueldo() desde la subclase
+
+console.log("Ejercicio 14 (16.)");
+document.write("<h3>Ejercicio 14 - Polimorfismo</h3>");
+
+// Clase PADRE
+class Empleado {
+    #nombre;
+    #sueldoBase;
+    constructor(nombre, sueldoBase) {
+        this.#nombre = nombre;
+        this.#sueldoBase = sueldoBase;
+    }
+    get nombre() {
+        return this.#nombre;
+    }
+    get sueldoBase() {
+        return this.#sueldoBase;
+    }
+    toString() {
+        return `Empleado: ${this.nombre}\nSueldo Base: S/. ${this.sueldoBase}`;
+    }
+}
+
+// Clases HIJAS
+class Programador extends Empleado {
+    #lenguaje;
+    constructor(nombre, sueldoBase, lenguaje) {
+        super(nombre, sueldoBase);
+        this.#lenguaje = lenguaje;
+    }
+
+    calcularSueldo() {
+        let bono = 1.1;
+        return this.sueldoBase * bono;
+    }
+
+    toString() {
+        return `${super.toString()}\nLenguaje: ${this.#lenguaje}\nSueldo con bono (10%): S/. ${this.calcularSueldo()}`;
+    }
+}
+
+// •	ProgramadorSenior sobreescribe calcularSueldo() con un bono del 25%
+class ProgramadorSenior extends Programador {
+    constructor(nombre, sueldoBase, lenguaje) {
+        super(nombre, sueldoBase, lenguaje);
+    }
+    calcularSueldo () {
+        return super.calcularSueldo() * 1.25; // Se añade el bono del 25%
+    }
+    toString() {
+        return `${super.toString().split('\nSueldo con bono')[0]}\nSueldo con bono (25%): S/. ${this.calcularSueldo()}`;
+    }
+}
+
+// Salida de datos
+const programador = new Programador("Saulo Cáceres", 30000, "JavaScript");
+console.log(programador.toString());
+document.write(`<pre style = "font-family: 'Times New Roman'">${programador.toString()}</pre>`);
+
+const programadorSenior = new ProgramadorSenior("Perpevua", 30000, "Python");
+console.log(programadorSenior.toString());
+document.write(`<pre style = "font-family: 'Times New Roman'">${programadorSenior.toString()}</pre>`);
